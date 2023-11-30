19afd05adb0f83d957ab45c8849c875ddd5e969b
Finlay Christ <RandomiaGaming@gmail.com>
2023-11-29 21:31:47 -0800

    Backup Commit

---
+++ Assets/Boxer/BoxerRig.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxerRig : MonoBehaviour
+{
+    private Transform Body;
+    public Transform Right_Arm;
+    public Transform Left_Arm;
+    public Transform Right_Hand;
+    public Transform Left_Hand;
+
+    void Start()
+    {
+        Body = transform;
+        Right_Hand.parent = Right_Arm;
+        Left_Hand.parent = Left_Arm;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
